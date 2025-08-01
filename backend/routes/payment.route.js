f7a3bae05dbaf1bf96b17a6795caa32a87d2f87d
akpobasa12345 <victorakpobasa2020@gmail.com>
2025-08-01 11:57:29 +0100

    fix vite

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -7,14 +7,7 @@ import {
 
 const router = express.Router();
 
-// @route   POST /api/payment/create-checkout-session
-// @desc    Create Stripe Checkout session
-// @access  Protected
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
-
-// @route   POST /api/payment/checkout-success
-// @desc    Handle successful payment
-// @access  Protected
 router.post("/checkout-success", protectRoute, checkoutSuccess);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -20,11 +19,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
-
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
@@ -36,14 +30,14 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
-  connectDB();
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
