0cd00168832e5de765b0c72d3c4a6f1138e47826
akpobasa12345 <victorakpobasa2020@gmail.com>
2025-08-08 05:17:47 +0100

    fix featured page

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -50,7 +50,7 @@ export const useCartStore = create((set, get) => ({
   addToCart: async (product) => {
     try {
       await axios.post("/cart", { productId: product._id });
-      toast.success("Product added to cart", { id: "cart" });
+      toast.success("Product added to cart");
 
       set((prevState) => {
         const existingItem = prevState.cart.find(
